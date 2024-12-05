c25c8deacb79afde04eadebce97069afaad65386
Pacholsc <charles.pacholski@erie.gov>
2024-12-05 14:38:37 -0500

    1st attempt IPv4 validation app

--- IPv4AddrValidationApp/Program.cs
+++ IPv4AddrValidationApp/Program.cs
@@ -14,36 +14,51 @@ The IPv4 address is provided as a string. You can assume that it only consists o
 if ipAddress consists of 4 numbers
 and if each ipAddress number has no leading zeroes
 and if each ipAddress number is in range 0 - 255
-
-then ipAddress is valid
-else ipAddress is invalid
-
 */
 using System;
 
-string phrase = "1.1.1.1";
+string phrase = "255.255.255.255";
 string[] bytes = phrase.Split('.');
+int i = 0;
+bool isByte1Valid = false;
+bool isByte2Valid = false;
+bool isByte3Valid = false;
+bool isByte4Valid = false;
+
 
 foreach (string bytePart in bytes)
 {
-    int i = 1; 
+    Console.WriteLine(bytePart);
     int byteInt = Convert.ToInt32(bytePart);
-    if (byteInt >= 1 && byteInt <= 255) {
-        // Console.WriteLine("Valid");
+    if ( byteInt >= 1 && byteInt <= 255 && !bytePart.StartsWith("0") ) {    
+        switch (i) 
+        {
+            case 0 : isByte1Valid = true; break;
+            case 1 : isByte2Valid = true; break;
+            case 2 : isByte3Valid = true; break;
+            case 3 : isByte4Valid = true; break;
+        }
     } else {
         switch (i) 
         {
-            case 1 : Console.WriteLine("First byte is errorneous"); break;
-            case 2 : Console.WriteLine("Second byte is errorneous"); break;
-            case 3 : Console.WriteLine("Thrid byte is errorneous"); break;
-            case 4 : Console.WriteLine("Fourth byte is errorneous"); break;
+            case 0 : Console.WriteLine("First byte is errorneous"); break;
+            case 1 : Console.WriteLine("Second byte is errorneous"); break;
+            case 2 : Console.WriteLine("Thrid byte is errorneous"); break;
+            case 3 : Console.WriteLine("Fourth byte is errorneous"); break;
         }
     }
     i++; 
     
 }
 
+bool IsIPV4 = (i == 4) ? true : false; 
 
+
+Console.WriteLine($"This IPv4 address consists of 4 numbers: {IsIPV4}");
+Console.WriteLine( $"Byte 1 is valid: {isByte1Valid}" );
+Console.WriteLine( $"Byte 2 is valid: {isByte2Valid}" );
+Console.WriteLine( $"Byte 3 is valid: {isByte3Valid}" );
+Console.WriteLine( $"Byte 4 is valid: {isByte4Valid}" );
 
 
 
