1c3d4328553f593729b77f8c759741ae4f9d81c6
Pacholsc <charles.pacholski@erie.gov>
2024-12-13 16:57:39 -0500

    Create circle calculator

--- printCircleDim/Program.cs
+++ printCircleDim/Program.cs
@@ -1,6 +1,27 @@
 ﻿// For more information see
 // https://learn.microsoft.com/en-us/training/modules/create-c-sharp-methods-parameters/3-exercise-understand-method-scope
 using System;
+
+double pi = 3.14159;
+
+Console.WriteLine("Enter the radius of a circle, as an integer:");
+int radius = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Circle with radius {radius}");
+calculateArea(radius);
+calculateCircumfirmence(radius);
+
+void calculateArea(int radius) {
+    double area = pi * (radius * radius);
+    Console.WriteLine($"Area = {area}");
+}
+
+void calculateCircumfirmence(int radius) {
+    double circumference = 2 * pi * radius;
+    Console.WriteLine($"Circumference = {circumference}");
+}
+/* 
+// Test of variable scope:
 string[] students = {"Jess", "Jerry", "James", "Jenny"};
 
 printStudents(students);
@@ -12,3 +33,5 @@ void printStudents(string[] students) {
         Console.WriteLine(student); 
     }
 }
+*/
+
