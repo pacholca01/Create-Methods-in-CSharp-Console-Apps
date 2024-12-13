7c0f4942ad8f994c8c4a2b10831f787c2a5616cd
Pacholsc <charles.pacholski@erie.gov>
2024-12-13 16:26:10 -0500

    Test of variable students scope

---
+++ printCircleDim/Program.cs
@@ -0,0 +1,14 @@
+﻿// For more information see
+// https://learn.microsoft.com/en-us/training/modules/create-c-sharp-methods-parameters/3-exercise-understand-method-scope
+using System;
+string[] students = {"Jess", "Jerry", "James", "Jenny"};
+
+printStudents(students);
+printStudents(new string[] {"Jamie", "Jared"});
+
+void printStudents(string[] students) {
+    foreach (var student in students)
+    {   
+        Console.WriteLine(student); 
+    }
+}
