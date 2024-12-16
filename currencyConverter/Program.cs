c8dc768d2399f23b4860beaae2e800fddd656f35
Pacholsc <charles.pacholski@erie.gov>
2024-12-16 17:11:15 -0500

    Create reverse word method

---
+++ currencyConverter/Program.cs
@@ -0,0 +1,20 @@
+﻿// For more info see 
+// https://learn.microsoft.com/en-us/training/modules/create-c-sharp-methods-return-values/3-exercise-create-methods-return-numbers
+
+using System; 
+
+double usd = 3.33;
+double rate = 23500;
+int vnd = ConvertUSDtoVND(usd);
+double usdBackConverted = ConvertVNDtoUSD(vnd);
+
+Console.WriteLine($"${usd} = {vnd} VND");
+Console.WriteLine($"{vnd} VND = ${usdBackConverted}");
+
+int ConvertUSDtoVND(double usdInput) {
+    return (int) (rate * usd);
+}
+
+double ConvertVNDtoUSD(int vnd) {
+    return vnd / rate;
+}
---
+++ reverseString/Program.cs
@@ -0,0 +1,23 @@
+﻿// For more info see 
+// https://learn.microsoft.com/en-us/training/modules/create-c-sharp-methods-return-values/4-exercise-create-methods-return-strings
+
+using System; 
+
+string pangram = "The quick brown fox jumps over the lazy dog";
+string palindrome = "racecar"; 
+string emordnilap = "desserts";
+
+Console.WriteLine(ReverseWord(pangram));
+Console.WriteLine(ReverseWord(palindrome));
+Console.WriteLine(ReverseWord(emordnilap));
+
+string ReverseWord(string word) 
+{
+    char[] charArray = new char[word.Length];
+    for (int i = 0; i < word.Length; i++) 
+    {
+        charArray[i] = word[word.Length-i-1];
+    }
+    string result = new string (charArray);
+    return result;
+}
